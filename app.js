94174cb49df1a620bc1c04871defd22bda34ec5f
Carlo-sh <cruaro@protonmail.com>
2023-10-18 17:51:19 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const container = document.getElementById('container');
+const sizeBtn = document.getElementById('size-btn');
+const randCol = () => Math.floor(Math.random() * 256);
+let mouseClick = false;
+
+sizeBtn.addEventListener('click', () => {
+	let size = prompt('Enter a size between 10 and 80:', '30');
+	if (container.hasChildNodes()) {
+		while (container.firstChild) {
+			container.removeChild(container.firstChild);
+		}
+	}
+	if (+size < 10 || +size > 80) {
+		alert('Invalid Grid Size');
+		return;
+	}
+	let gridSide = +size;
+	for (let i = 0; i < gridSide ** 2; i++) {
+		let square = document.createElement('div');
+		let bright = 50;
+		square.addEventListener('mousedown', () => {
+			if (square.style.backgroundColor === '') {
+				square.style.backgroundColor = `rgb(${randCol()}, ${randCol()}, ${randCol()})`;
+			}
+			mouseClick = true;
+		});
+
+		square.addEventListener('mouseup', () => (mouseClick = false));
+
+		square.addEventListener('mouseover', () => {
+			if (mouseClick) {
+				if (square.style.backgroundColor === '') {
+					square.style.backgroundColor = `rgb(${randCol()}, ${randCol()}, ${randCol()})`;
+				} else {
+					square.style.filter = `brightness(${bright === 0 ? bright : (bright -= 5)}%)`;
+				}
+			}
+		});
+		container.appendChild(square);
+	}
+	container.style.gridTemplate = `repeat(${gridSide}, 1fr) / repeat(${gridSide}, 1fr)`;
+});
